2185285ce95072c1ad26347d16ec5561d2bca310
lvqchau <lvqchau@apcs.vn>
2019-08-09 21:39:26 +0700

    Merging

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,31 @@
 import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from "@angular/core";
-
+import { FlexLayoutModule } from "@angular/flex-layout";
+import { MatButtonModule } from "@angular/material/button";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
-import { CarouselModule } from "ngx-carousel-lib";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatTabsModule } from "@angular/material/tabs";
+import { MatSelectModule } from "@angular/material/select";
+import { SlickCarouselModule } from "ngx-slick-carousel";
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, CarouselModule],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    BrowserModule,
+    FlexLayoutModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatMenuModule,
+    MatTabsModule,
+    MatSelectModule,
+    SlickCarouselModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
